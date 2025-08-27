b4257006cddba6971e1178db9ab32dc64decaa2f
Bruna <brunabarbozasofia@gmail.com>
2025-08-27 15:35:45 -0300

    criando no posts tambem

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,13 @@
+from django.views.generic import ListView
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+def lista_posts(request):
+    posts = Post.objects.all()  # busca todos os heróis do banco
+    return render(request, "posts/lista_posts.html", {"posts": posts})
+
+class HeroListView(ListView):
+    model = Post
+    template_name = "posts/lista_posts.html"
+    context_object_name = "posts"
+    
--- superbook/urls.py
+++ superbook/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('heroes/', include('heroes.urls')),  # rotas do app heroes
-    # path('posts/', include('posts.urls')),    # rotas do app posts
+    path('posts/', include('posts.urls')),    # rotas do app posts
 ]
