ef6e84b4bc4b9682ab7e85aef29c403e153a60b7
Bruna <brunabarbozasofia@gmail.com>
2025-09-03 16:44:28 -0300

    crianco forms e validacaoes

--- heroes/urls.py
+++ heroes/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views import lista_herois, HeroListView
+from . import views
 
 urlpatterns = [
     path('lista/', lista_herois, name='lista_herois'),
     path('cbv-lista/', HeroListView.as_view(), name='cbv_lista_herois'),
+    path('contato/', views.contato_view, name='contato'),
+    path('novo/', views.criar_heroi, name='criar_heroi'),
 ]
 
