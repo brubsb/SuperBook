12feda61e0732ccc7215e480cadeb781e3713e65
Bruna <brunabarbozasofia@gmail.com>
2025-09-11 14:45:58 -0300

    arrumando email

--- villains/models.py
+++ villains/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Villain(models.Model):
     codinome = models.CharField(max_length=50, unique=True)
     nome_real = models.CharField(max_length=100, blank=True, null=True)
-    email_contato = models.CharField(max_length=100, blank=True, null=True)
+    email_contato = models.EmailField(blank=True, null=True)
     poder_principal = models.CharField(max_length=100)
     cidade = models.CharField(max_length=100)
     historia = models.TextField(blank=True, null=True)
